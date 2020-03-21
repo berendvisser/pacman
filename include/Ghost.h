eb3ffd1f35d94368b1a8708550647f4e0ce6fed3
User <berendvisser@outlook.com>
2020-03-21 22:23:57 +0100

    added documentation for ghost

--- include/Ghost.h
+++ include/Ghost.h
@@ -1,3 +1,8 @@
+/*
+ *  Created on: March, 2020
+ *      Author: Berend Visser
+ *       Group: 30
+ */
 #pragma once
 
 #include "MovableEntity.h"
@@ -10,136 +15,38 @@ class Ghost : public MovableEntity, public Eatable
 {
 public:
 	/*Constructor*/
-	Ghost(Type ghostType, Board* tmpBoard) :MovableEntity(tmpBoard, ghostType), Eatable(ghostType), Entity(ghostType)
-	{
-		this->setScoreOnCollision(-1); //set score on collision
-		this->isScared = false;
-		this->normalType = ghostType;
-		std::srand(std::time(NULL));
-		
-	}
+	Ghost(Type ghostType, Board* tmpBoard);
 
 	/*Deconstructor*/
-	~Ghost(){}
+	~Ghost();
 
+	/*Set new direction for ghost*/
+	void setNewDirection();
 
-
-	void setNewDirection()
-	{		
-		this->updateScaredState();
-
-
-		Position freePosition; //free position with respect to current spot
-		unsigned freeDirections = 0; //number of directions to choose from
-
-		for (unsigned i= 0; i < 4; i++)//loop to check all four direciton around current spot
-		{
-			freePosition = this->getNextPosition(static_cast<Direction>(i)); //check first direction
-			
-			if (!map->isWall(freePosition))//check if there is not a wall in current position
-			{
-				freeDirections++; //increase number of free positions if no wall is found
-				
-			}
-		}
-
-		if (freeDirections != 2) //if 1, 3 or 4 directions are possible 
-		{
-			this->setRandomDirection(); //pick a random direction out of the possible directions
-		}
-		else //if only two directions are possible
-		{	
-			Direction OppositeDir = this->getOppositeDirection(); //get direction opposite to current direction
-			this->setRandomDirection(); //pick random direction
-
-			while (this->getDirection() == OppositeDir) //keep checking for a new direction that is not opposite to current direction
-			{
-				this->setRandomDirection(); //set new random direction
-			}
-			
-		}
-
-
-	}
-
-	/*Make ghost scared for 50 ticks*/
-	void setScared(bool scared)
-	{
-	
-		this->isScared = scared;
-		this->ticksLeftScared = 50;
-		this->setScoreOnCollision(20);
-
-
-
-	}
+	/*Make ghost scared ticks*/
+	void setScared(bool scared);
 
 	/*check if ghost is currently scared*/
-	bool getGhostScared()
-	{
-		return this->isScared;
-	}
+	bool getGhostScared();
 	
 
 
 protected:
-	/*Update scared state*/
-	void updateScaredState()
-	{
-		
-		if (this->isScared && this->ticksLeftScared > 0)
-		{
-			this->ticksLeftScared--;
-			this->entityType.type = SCARED;
-		}
-		else
-		{
-			this->setScared(false);
-			this->entityType.type = normalType;
-			this->setScoreOnCollision(-1);
-			
-		}
 
-	}
+	/*Update scared state*/
+	void updateScaredState();
 
 	/*Return opposite direction of Ghost*/
-	Direction getOppositeDirection()
-	{
-		switch (this->getDirection()) //switch on current direction
-		{
-			//return inverse
-		case UP:
-			return DOWN;
-			break;
-		case DOWN:
-			return UP;
-			break;
-		case LEFT:
-			return RIGHT;
-			break;
-		case RIGHT:
-			return LEFT;
-			break;
-		default:
-			return LEFT;
-		}
-			
-				
-	}
+	Direction getOppositeDirection();
+
 private:
+
 	/*sets new random position*/
-	void setRandomDirection()
-	{
+	void setRandomDirection();
 
-		//keep setting new random direction until new direction is valid
-		while (!this->setDirection(static_cast<Direction>(std::rand() % 4)))
-		{
-		}
-	}
-
-	bool isScared;
-	unsigned ticksLeftScared;
-	Type normalType;
+	bool isScared; //state of whether the ghost is scared
+	unsigned ticksLeftScared; //How many game ticks the ghost will be scared
+	Type normalType; //What the type of the ghost is when it is not scared(ie inky)
 	
 
 };
--- src/Ghost.cpp
+++ src/Ghost.cpp
@@ -1,145 +1,126 @@
+/*
+ *  Created on: March, 2020
+ *      Author: Berend Visser
+ *       Group: 30
+ */
 #pragma once
+#include "Ghost.h"
 
-#include "MovableEntity.h"
-#include "Eatable.h"
-#include <cstdlib>
-#include <ctime>
-
-
-class Ghost : public MovableEntity, public Eatable
+Ghost::Ghost(Type ghostType, Board* tmpBoard) :MovableEntity(tmpBoard, ghostType), Eatable(ghostType), Entity(ghostType)
 {
-public:
-	/*Constructor*/
-	Ghost(Type ghostType, Board* tmpBoard) :MovableEntity(tmpBoard, ghostType), Eatable(ghostType), Entity(ghostType)
+	this->setScoreOnCollision(-1); //set score on collision
+	this->isScared = false; //set initial scared state
+	this->normalType = ghostType; //set type to return to after scared state
+	std::srand(std::time(NULL)); //set seed for random movement behaviour	
+}
+
+/*Deconstructor*/
+Ghost::~Ghost()
+{
+}
+
+void Ghost::setNewDirection()
+{		
+	this->updateScaredState();
+
+
+	Position freePosition; //free position with respect to current spot
+	unsigned freeDirections = 0; //number of directions to choose from
+
+	for (unsigned i= 0; i < 4; i++)//loop to check all four direciton around current spot
 	{
-		this->setScoreOnCollision(-1); //set score on collision
-		this->isScared = false;
-		this->normalType = ghostType;
-		std::srand(std::time(NULL));
-		
-	}
-
-	/*Deconstructor*/
-	~Ghost(){}
-
-
-
-	void setNewDirection()
-	{		
-		this->updateScaredState();
-
-
-		Position freePosition; //free position with respect to current spot
-		unsigned freeDirections = 0; //number of directions to choose from
-
-		for (unsigned i= 0; i < 4; i++)//loop to check all four direciton around current spot
-		{
-			freePosition = this->getNextPosition(static_cast<Direction>(i)); //check first direction
+		freePosition = this->getNextPosition(static_cast<Direction>(i)); //check first direction
 			
-			if (!map->isWall(freePosition))//check if there is not a wall in current position
-			{
-				freeDirections++; //increase number of free positions if no wall is found
+		if (!map->isWall(freePosition))//check if there is not a wall in current position
+		{
+			freeDirections++; //increase number of free positions if no wall is found
 				
-			}
 		}
+	}
 
-		if (freeDirections != 2) //if 1, 3 or 4 directions are possible 
+	if (freeDirections != 2) //if 1, 3 or 4 directions are possible 
+	{
+		this->setRandomDirection(); //pick a random direction out of the possible directions
+	}
+	else //if only two directions are possible
+	{	
+		Direction OppositeDir = this->getOppositeDirection(); //get direction opposite to current direction
+		this->setRandomDirection(); //pick random direction
+
+		while (this->getDirection() == OppositeDir) //keep checking for a new direction that is not opposite to current direction
 		{
-			this->setRandomDirection(); //pick a random direction out of the possible directions
+			this->setRandomDirection(); //set new random direction
 		}
-		else //if only two directions are possible
-		{	
-			Direction OppositeDir = this->getOppositeDirection(); //get direction opposite to current direction
-			this->setRandomDirection(); //pick random direction
-
-			while (this->getDirection() == OppositeDir) //keep checking for a new direction that is not opposite to current direction
-			{
-				this->setRandomDirection(); //set new random direction
-			}
 			
-		}
-
-
 	}
 
-	/*Make ghost scared for 50 ticks*/
-	void setScared(bool scared)
-	{
+
+}
+
+void Ghost::setScared(bool scared)
+{
 	
-		this->isScared = scared;
-		this->ticksLeftScared = 50;
-		this->setScoreOnCollision(20);
+	this->isScared = scared; //set state of ghost
+	this->ticksLeftScared = 50; //set time of being scared
+	this->setScoreOnCollision(20); //set score for eating ghost
 
 
 
-	}
+}
 
-	/*check if ghost is currently scared*/
-	bool getGhostScared()
-	{
-		return this->isScared;
-	}
-	
+bool Ghost::getGhostScared()
+{
+	return this->isScared; //return scared state of ghost
+}
 
-
-protected:
-	/*Update scared state*/
-	void updateScaredState()
-	{
+void Ghost::updateScaredState()
+{
 		
-		if (this->isScared && this->ticksLeftScared > 0)
-		{
-			this->ticksLeftScared--;
-			this->entityType.type = SCARED;
-		}
-		else
-		{
-			this->setScared(false);
-			this->entityType.type = normalType;
-			this->setScoreOnCollision(-1);
+	if (this->isScared && this->ticksLeftScared > 0) //check if ghost is scared and time of being scared has not run out yet
+	{
+		this->ticksLeftScared--; //decrement timer for being scared
+		this->entityType.type = SCARED; //set ghosttype to scared
+	}
+	else
+	{
+		this->setScared(false); //Set state of ghost to not scared when timer has run out or ghost have been reset
+		this->entityType.type = this->normalType;//set ghost back to initial type
+		this->setScoreOnCollision(-1); //set score on collision back to normal
 			
-		}
-
 	}
 
-	/*Return opposite direction of Ghost*/
-	Direction getOppositeDirection()
+}
+
+Direction Ghost::getOppositeDirection()
+{
+	switch (this->getDirection()) //switch on current direction
 	{
-		switch (this->getDirection()) //switch on current direction
-		{
-			//return inverse
-		case UP:
-			return DOWN;
-			break;
-		case DOWN:
-			return UP;
-			break;
-		case LEFT:
-			return RIGHT;
-			break;
-		case RIGHT:
-			return LEFT;
-			break;
-		default:
-			return LEFT;
-		}
+		//return inverse
+	case UP:
+		return DOWN;
+		break;
+	case DOWN:
+		return UP;
+		break;
+	case LEFT:
+		return RIGHT;
+		break;
+	case RIGHT:
+		return LEFT;
+		break;
+	default:
+		return LEFT;
+	}
 			
 				
-	}
-private:
-	/*sets new random position*/
-	void setRandomDirection()
+}
+
+void Ghost::setRandomDirection()
+{
+
+	//keep setting new random direction until new direction is valid
+	while (!this->setDirection(static_cast<Direction>(std::rand() % 4)))
 	{
-
-		//keep setting new random direction until new direction is valid
-		while (!this->setDirection(static_cast<Direction>(std::rand() % 4)))
-		{
-		}
 	}
+}
 
-	bool isScared;
-	unsigned ticksLeftScared;
-	Type normalType;
-	
-
-};
